629bb609d9642f58e4a16c2fff61e4f2127130f9
Nafis Shamsevaliev <shamsic2006@gmail.com>
2024-12-23 19:17:10 +0300

    3.2-crud-correction

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django-filters',
 
     'logistic',
 ]
@@ -129,3 +130,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumber',
+    'PAGE_SIZE': 2
+}
